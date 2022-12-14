3204fa6f5c018b5afa4954e5a20032d7cdc49424
YulianaPelozo <120428407+YulianaPelozo@users.noreply.github.com>
2022-12-14 20:49:03 -0300

    Se agrego modelado de noticia, comentario, categoría, usuario

---
+++ proyecto/myapp/modelos/comentario.py
@@ -0,0 +1,26 @@
+class comentario:
+    def __init__(
+        self,id,idNoticia, nombreUsuario, idUsuario, fechaComentario, comentario):
+        self.id = id
+        self.idNoticia = idNoticia
+        self.nombreUsuario = nombreUsuario
+        self.idUsuario = idUsuario
+        self.fechaComentario = fechaComentario
+        self.comentario = comentario
+
+
+        def obtenerListadoComentario(self):
+        print(f"este es listado para obtener comentario")
+
+    def obtenerComentarioPorId(self):
+        print(f"este es comentario por id")
+
+    def crearComentario(self):
+        print(f"")
+
+    def modificarComentario(self):
+        print(f"")
+
+    def eliminarComentario(self):
+        print(f"")
+
---
+++ proyecto/myapp/modelos/noticia.py
@@ -0,0 +1,28 @@
+class noticia:
+    def __init__(
+        self,id, titulo, nombreUsuario, idUsuario, fechaPublicacion, rutaImagen, idCategoria):
+        self.id = id
+        self.titulo = titulo
+        self.nombreUsuario = nombreUsuario
+        self.idUsuario = idUsuario
+        self.fechaPublicacion = fechaPublicacion
+        self.rutaImagen = rutaImagen
+        self.idCategoria = idCategoria
+
+
+
+    def obtenerListadoNoticia(self):
+        print(f"este es listado para obtener noticias")
+
+    def obtenerNoticiaPorId(self):
+        print(f"este es noticia por id")
+
+    def crearNoticia(self):
+        print(f"")
+
+    def modificarNoticia(self):
+        print(f"")
+
+    def eliminarNoticia(self):
+        print(f"")
+
